63ab209426041a0a069860cc5e33cacc05a52fe5
jc <jaechol.hong@outlook.com>
2026-02-11 00:18:29 +0100

    database setting fix in database.js

--- config/database.js
+++ config/database.js
@@ -1,40 +1,34 @@
 const path = require('path');
 
-/**
- * @param {Object} p
- * @param {Function} p.env
- */
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const dbUrl = env('DATABASE_URL');
 
-  const connections = {
-    // This will be used on Render
-    postgres: {
+  // If DATABASE_URL exists (Render environment), use Postgres
+  if (dbUrl) {
+    return {
       connection: {
-        connectionString: env('DATABASE_URL'),
-        ssl: env.bool('DATABASE_SSL', false) && { 
-          rejectUnauthorized: false 
+        client: 'postgres',
+        connection: {
+          connectionString: dbUrl,
+          ssl: {
+            rejectUnauthorized: false, // Required for Render Postgres
+          },
         },
+        pool: { min: 2, max: 10 },
+        acquireConnectionTimeout: 60000,
       },
-      pool: { 
-        min: env.int('DATABASE_POOL_MIN', 2), 
-        max: env.int('DATABASE_POOL_MAX', 10) 
-      },
-    },
-    // This stays for your local work
-    sqlite: {
+    };
+  }
+
+  // Otherwise, fallback to SQLite (Local environment)
+  return {
+    connection: {
+      client: 'sqlite',
       connection: {
         filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
       },
       useNullAsDefault: true,
-    },
-  };
-
-  return {
-    connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      acquireConnectionTimeout: 60000,
     },
   };
 };
